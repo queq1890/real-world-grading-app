7e1a885c38b66dfccbe863cc78dc7fe30c295eba
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-02-07 14:40:16 +0700

    part1

---
+++ prisma/migrations/20210207070556_init/migration.sql
@@ -0,0 +1,76 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('STUDENT', 'TEACHER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "social" JSONB,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "courseDetails" TEXT,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CourseEnrollment" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "UserRole" NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("userId","courseId")
+);
+
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "courseId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TestResult" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "result" INTEGER NOT NULL,
+    "studentId" INTEGER NOT NULL,
+    "graderId" INTEGER NOT NULL,
+    "testId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "CourseEnrollment" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CourseEnrollment" ADD FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Test" ADD FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TestResult" ADD FOREIGN KEY ("studentId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TestResult" ADD FOREIGN KEY ("graderId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TestResult" ADD FOREIGN KEY ("testId") REFERENCES "Test"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/seed.ts
+++ src/seed.ts
@@ -1,17 +1,117 @@
-import { PrismaClient } from '@prisma/client'
-import { add } from 'date-fns'
+import { PrismaClient } from '@prisma/client';
+import { add } from 'date-fns';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // A `main` function so that we can use async/await
-async function main() {}
+async function main() {
+  const weekFromNow = add(new Date(), { days: 7 });
+  const twoWeekFromNow = add(new Date(), { days: 14 });
+  const monthFromNow = add(new Date(), { days: 28 });
+
+  const grace = await prisma.user.create({
+    data: {
+      email: 'grace@hey.com',
+      firstName: 'Grace',
+      lastName: 'Bell',
+      social: {
+        facebook: 'gracebell',
+        twitter: 'therealgracebell',
+      },
+    },
+  });
+
+  const course = await prisma.course.create({
+    data: {
+      name: 'CRUD with Prisma',
+      tests: {
+        create: [
+          {
+            date: weekFromNow,
+            name: 'First test',
+          },
+          {
+            date: twoWeekFromNow,
+            name: 'Second test',
+          },
+          {
+            date: monthFromNow,
+            name: 'Final exam',
+          },
+        ],
+      },
+      members: {
+        create: {
+          role: 'TEACHER',
+          user: {
+            connect: {
+              email: 'grace@hey.com',
+            },
+          },
+        },
+      },
+    },
+    // include relations in the result
+    include: {
+      tests: true,
+    },
+  });
+
+  const shakuntala = await prisma.user.create({
+    data: {
+      email: 'devi@prisma.io',
+      firstName: 'Shakuntala',
+      lastName: 'Devi',
+      courses: {
+        create: {
+          role: 'STUDENT',
+          course: {
+            connect: {
+              id: grace.id,
+            },
+          },
+        },
+      },
+    },
+  });
+  const david = await prisma.user.create({
+    data: {
+      email: 'david@prisma.io',
+      firstName: 'David',
+      lastName: 'Deutsch',
+      courses: {
+        create: {
+          role: 'STUDENT',
+          course: {
+            connect: { id: course.id },
+          },
+        },
+      },
+    },
+  });
+
+  // await prisma.testResult.create({
+  //   data: {
+  //     gradedBy: {
+  //       connect: { email: grace.email },
+  //     },
+  //     student: {
+  //       connect: { email: shakuntala.email },
+  //     },
+  //     test: {
+  //       connect: { id: test.id },
+  //     },
+  //     result: 950,
+  //   },
+  // });
+}
 
 main()
   .catch((e: Error) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
     // Disconnect Prisma Client
-    await prisma.disconnect()
-  })
+    await prisma.$disconnect();
+  });
