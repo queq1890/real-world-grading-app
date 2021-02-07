8ee8a62faa81c612723ad11172daadb4e5d5f169
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-02-07 16:13:29 +0700

    part2

---
+++ src/plugins/users.ts
@@ -0,0 +1,166 @@
+import Hapi from '@hapi/hapi';
+import Joi from '@hapi/joi';
+import Boom from '@hapi/boom';
+
+interface UserInput {
+  firstName: string;
+  lastName: string;
+  email: string;
+  social: {
+    facebook?: string;
+    twitter?: string;
+    github?: string;
+    website?: string;
+  };
+}
+
+const userInputValidator = Joi.object({
+  firstName: Joi.string().alter({
+    create: (schema) => schema.required(),
+    update: (schema) => schema.optional(),
+  }),
+  lastName: Joi.string().alter({
+    create: (schema) => schema.required(),
+    update: (schema) => schema.optional(),
+  }),
+  email: Joi.string()
+    .email()
+    .alter({
+      create: (schema) => schema.required(),
+      update: (schema) => schema.optional(),
+    }),
+  social: Joi.object({
+    facebook: Joi.string().optional(),
+    twitter: Joi.string().optional(),
+    github: Joi.string().optional(),
+    website: Joi.string().optional(),
+  }).optional(),
+});
+const createUserValidator = userInputValidator.tailor('create');
+const updateUserValidator = userInputValidator.tailor('update');
+
+async function getUserHandler(request: Hapi.Request, h: Hapi.ResponseToolkit) {
+  const { prisma } = request.server.app;
+  const userId = parseInt(request.params.userId, 10);
+  try {
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      return h.response().code(404);
+    } else {
+      return h.response(user).code(200);
+    }
+  } catch (err) {
+    console.log(err);
+    return Boom.badImplementation();
+  }
+}
+
+async function createUserHandler(
+  request: Hapi.Request,
+  h: Hapi.ResponseToolkit
+) {
+  const { prisma } = request.server.app;
+  const payload = request.payload as UserInput;
+  try {
+    const createdUser = await prisma.user.create({
+      data: {
+        firstName: payload.firstName,
+        lastName: payload.lastName,
+        email: payload.email,
+        social: JSON.stringify(payload.social),
+      },
+      select: { id: true },
+    });
+    return h.response(createdUser).code(201);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function updateUserHandler(
+  request: Hapi.Request,
+  h: Hapi.ResponseToolkit
+) {
+  const { prisma } = request.server.app;
+  const userId = parseInt(request.params.userId, 10);
+  const payload = request.payload as Partial<UserInput>;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: payload,
+    });
+
+    return h.response(updatedUser).code(200);
+  } catch (err) {
+    console.error(err);
+    return h.response().code(500);
+  }
+}
+
+async function deleteUserHandler(
+  request: Hapi.Request,
+  h: Hapi.ResponseToolkit
+) {
+  const { prisma } = request.server.app;
+  const userId = parseInt(request.params.userId, 10);
+  try {
+    await prisma.user.delete({ where: { id: userId } });
+    return h.response().code(204);
+  } catch (err) {
+    console.log(err);
+    return h.response().code(500);
+  }
+}
+
+const usersPlugin = {
+  name: 'app/users',
+  dependencies: ['prisma'],
+  register: async function (server: Hapi.Server) {
+    server.route([
+      {
+        method: 'GET',
+        path: '/users/{userId}',
+        handler: getUserHandler,
+        options: {
+          validate: {
+            params: Joi.object({
+              userId: Joi.number().integer(),
+            }),
+          },
+        },
+      },
+
+      {
+        method: 'POST',
+        path: '/users',
+        handler: createUserHandler,
+        options: { validate: { payload: createUserValidator } },
+      },
+      {
+        method: 'PUT',
+        path: '/users/{userId}',
+        handler: updateUserHandler,
+        options: {
+          validate: {
+            params: Joi.object({
+              userId: Joi.number().integer(),
+            }),
+            payload: updateUserValidator,
+          },
+        },
+      },
+      {
+        method: 'DELETE',
+        path: '/users/{userId}',
+        handler: deleteUserHandler,
+        options: {
+          validate: { params: Joi.object({ userId: Joi.number().integer() }) },
+        },
+      },
+    ]);
+  },
+};
+
+export default usersPlugin;
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import Hapi from '@hapi/hapi';
+
+import status from './plugins/status';
+import prisma from './plugins/prisma';
+import users from './plugins/users';
+
+const server: Hapi.Server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || 'localhost',
+});
+
+export async function createServer(): Promise<Hapi.Server> {
+  await server.register([prisma, status, users]);
+  await server.initialize();
+  return server;
+}
+
+export async function startServer(server: Hapi.Server): Promise<Hapi.Server> {
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+  return server;
+}
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+export async function start(): Promise<Hapi.Server> {
+  const server = await createServer();
+  return await startServer(server);
+}
